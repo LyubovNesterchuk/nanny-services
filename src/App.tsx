f029cb4bc618b0720fb457a6f2ec10e392e223f5
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-12-15 08:56:29 +0200

    add sprite, hero img

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <a href="#">
+          <img className="logo" alt="Nanny services logo" />
+        </a>
+        {/* <Home/>
+        <Nannies/> */}
+        <button> Log In </button>
+        <button> Registration </button>
+      </div>
+      <h1 className="hero-title">Make Life Easier for the Family:</h1>
+      <p className="title-description">Find Babysitters Online for All Occasions</p>
+      <button> Get started </button>
+    </>
+  )
+}
+
+export default App
